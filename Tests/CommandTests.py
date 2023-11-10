a97b6f46d4e47bf0406c8fbdb3b83e58bc49559b
William Sautour <wsautour@gmail.com>
2023-11-10 09:43:55 +0100

    Little refacto

--- Tests/CommandTests.py
+++ Tests/CommandTests.py
@@ -1,16 +1,17 @@
 import unittest
-import MissionControl
-import Domain
+from MissionControl.main import MyCommandSender, MyCommandReceiver, MyCommunicationProtocol
+from Domain.Exploration import planet
+from Domain.MissionRover import rover, instruction
 
 
 class CommandTests(unittest.TestCase):
     def setUp(self):
         self.server_address = ('127.0.0.1', 12345)
-        self.sender = MissionControl.main.MyCommandSender()
-        self.receiver = MissionControl.main.MyCommandReceiver()
-        self.protocol = MissionControl.main.MyCommunicationProtocol(self.sender, self.receiver)
-        self.mars = Domain.Exploration.planet.Planet(5, 5, None)
-        self.curiosity = Domain.MissionRover.rover.Rover(0, 0, 'N')
+        self.sender = MyCommandSender()
+        self.receiver = MyCommandReceiver()
+        self.protocol = MyCommunicationProtocol(self.sender, self.receiver)
+        self.mars = planet.Planet(5, 5, None)
+        self.curiosity = rover.Rover(0, 0, 'N')
 
     def testInitAndCloseProtocol(self):
         self.protocol.initialize_server(self.server_address)
@@ -21,11 +22,10 @@ class CommandTests(unittest.TestCase):
 
     def testSendAndReceive(self):
         self.protocol.initialize_server(self.server_address)
-        instructions = Domain.MissionRover.instruction.Instruction()
-
+        instructions = instruction.Instruction()
         instructions.add_instruction()
-        self.sender.send_command(instructions)
 
+        self.sender.send_command(instructions)
         data = self.receiver.receive_command()
         unittest.TestCase.assertEqual(self, data, instructions)
         self.protocol.close_connection()
