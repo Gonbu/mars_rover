b2c1dbd710937b57046e8cad62dfdae4db1981fa
William Sautour <wsautour@gmail.com>
2023-11-10 11:00:32 +0100

    save before rebase

--- Tests/CommandTests.py
+++ Tests/CommandTests.py
@@ -1,34 +1,44 @@
 import unittest
-from MissionControl.main import MyCommandSender, MyCommandReceiver, MyCommunicationProtocol
+import Rover.main as r
+import MissionControl.main as mc
 from Domain.Exploration import planet
 from Domain.MissionRover import rover, instruction
 
 
+class TestServer:
+    def __init__(self):
+        self.server_address = ('127.0.0.1', 12345)
+
+        self.sender = r.MyCommandSender()
+        self.receiver = r.MyCommandReceiver()
+        self.protocol = r.MyCommunicationProtocol(self.sender, self.receiver)
+
+
+class TestClient:
+    def __init__(self):
+        self.server_address = ('127.0.0.1', 12345)
+
+        self.sender = mc.MyCommandSender()
+        self.receiver = mc.MyCommandReceiver()
+        self.protocol = mc.MyCommunicationProtocol(self.sender, self.receiver)
+
+
 class CommandTests(unittest.TestCase):
     def setUp(self):
-        self.server_address = ('127.0.0.1', 12345)
-        self.sender = MyCommandSender()
-        self.receiver = MyCommandReceiver()
-        self.protocol = MyCommunicationProtocol(self.sender, self.receiver)
+        self.server = TestServer()
+        self.client = TestClient()
         self.mars = planet.Planet(5, 5, None)
         self.curiosity = rover.Rover(0, 0, 'N')
 
     def testInitAndCloseProtocol(self):
-        self.protocol.initialize_server(self.server_address)
-        unittest.TestCase.assertEqual(self, self.protocol.client_address, self.server_address)
+        self.server.protocol.initialize_server(self.server.server_address)
+        unittest.TestCase.assertEqual(self, self.server.protocol.client_socket, None)
 
-        self.protocol.close_connection()
-        unittest.TestCase.assertEqual(self, self.protocol.client_socket, None)
+
+
 
     def testSendAndReceive(self):
-        self.protocol.initialize_server(self.server_address)
-        instructions = instruction.Instruction()
-        instructions.add_instruction()
-
-        self.sender.send_command(instructions)
-        data = self.receiver.receive_command()
-        unittest.TestCase.assertEqual(self, data, instructions)
-        self.protocol.close_connection()
+        pass
 
 
 if __name__ == '__main__':
