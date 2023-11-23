b3f26c4b8c1de366177dde192a7a1bedd177983d
GMICHON <gauthier.michon@ynov.com>
2023-11-23 14:42:02 +0100

    rover.py delete encapsulation violation

--- Domain/MissionRover/command.py
+++ Domain/MissionRover/command.py
@@ -9,15 +9,11 @@ class Command:
         self.__backward = 'B'
         self.__left = 'L'
         self.__right = 'R'
-        self.again = True
         self.__is_valid = is_valid
 
     def add_command(self):
         self.__command_order = []
         commands_string = input('Enter commands: ').replace(" ", "")
-        if "Q" in commands_string :
-            self.again = False
-            return
         commands = list(commands_string)
         self.__is_valid = all(element == self.__forward or element == self.__backward or element == self.__left or element == self.__right for element in commands)
         
@@ -26,10 +22,10 @@ class Command:
                 self.__command_order.append(command)
         else:
             print('Invalid command list')
-        pass
+        
 
     def exec_commands(self, planet, rover):
-        if self.__is_valid == False:
+        if not self.is__valid:
             return rover, None
         for command in self.__command_order:
             actual_rover = rover
