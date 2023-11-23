2fc4dff477fd9b47177177d90313c8d9d801ea26
William Sautour <wsautour@gmail.com>
2023-11-23 17:15:58 +0100

    First functional test batch

--- Tests/CommandTests.py
+++ Tests/CommandTests.py
@@ -1,30 +1,28 @@
-# import sys
-# sys.path.append('../Rover')
-
-import unittest
+from unittest import TestCase, mock, main
 import time
 import threading
 import Rover.main as r
 import MissionControl.main as mc
-from Domain.Exploration import planet
-from Domain.MissionRover import rover, instruction
+from Domain.Topologie import position
+from Domain.Exploration import planet, obstacle
+from Domain.MissionRover import rover, command
 
 
 class TestServer:
     def __init__(self):
-        self.sender = r.MyCommandSender()
-        self.receiver = r.MyCommandReceiver()
+        self.sender = r.SenderToMissionControl()
+        self.receiver = r.ReceiverFromMissionControl()
         self.protocol = r.MyCommunicationProtocol(self.sender, self.receiver)
 
 
 class TestClient:
     def __init__(self):
-        self.sender = mc.MyCommandSender()
-        self.receiver = mc.MyCommandReceiver()
+        self.sender = mc.SenderToRover()
+        self.receiver = mc.ReceiverFromRover()
         self.protocol = mc.MyCommunicationProtocol(self.sender, self.receiver)
 
 
-class CommandTests(unittest.TestCase):
+class CommandTests(TestCase):
     def setUp(self):
         self.server_address = ('127.0.0.1', 12345)
         self.server = TestServer()
@@ -43,22 +41,56 @@ class CommandTests(unittest.TestCase):
         # Connect the client to the server
         self.client.protocol.establish_connection(self.server_address)
 
-    def testSendCommand(self):
-        # Send a command from the client to the server
-        self.client.protocol.send_command(self.client.protocol.client_socket, "test")
+    def testAddCommandsValidInput(self):
+        commands = command.Command()
 
-        # Receive the command on the server side
-        command = self.server.protocol.receive_command(self.server.protocol.client_socket)
+        # Use mock to simulate user input
+        with mock.patch('builtins.input', return_value='FFLBR'):
+            commands.add_command()
 
-        # Check if the command is the same as the one sent
-        self.assertEqual(command, "test")
+            self.assertEqual(commands._Command__command_order, ['F', 'F', 'L', 'B', 'R'])
+            self.assertEqual(commands._Command__is_valid, True)
 
-    def testSendAndReceive(self):
-        # Send a command from the client to the server and receive the response
-        response = self.client.protocol.send_and_receive(self.client.protocol.client_socket, "test")
+    def testAddCommandsInvalidInput(self):
+        commands = command.Command()
 
-        # Check if the response is the same as the one sent
-        self.assertEqual(response, "test")
+        # Use mock to simulate user input
+        with mock.patch('builtins.input', return_value='invalid_input'):
+            commands.add_command()
+
+            self.assertEqual(commands._Command__command_order, [])
+            self.assertEqual(commands._Command__is_valid, False)
+
+    def testExecCommandsValid(self):
+        commands = command.Command(['F', 'F', 'L', 'B', 'R'], True)
+
+        self.curiosity, obstacles = commands.exec_commands(self.mars, self.curiosity)
+
+        self.assertEqual(self.curiosity._Rover__position._Position__x._Coordinate__value, 1)
+        self.assertEqual(self.curiosity._Rover__position._Position__y._Coordinate__value, 2)
+        self.assertEqual(self.curiosity._Rover__orientation._Orientation__orientation, 'N')
+        self.assertIsNone(obstacles)
+
+    def testExecCommandsInvalid(self):
+        commands = command.Command(['invalid_input'], False)
+
+        self.curiosity, obstacles = commands.exec_commands(self.mars, self.curiosity)
+
+        self.assertEqual(self.curiosity._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.curiosity._Rover__position._Position__y._Coordinate__value, 0)
+        self.assertEqual(self.curiosity._Rover__orientation._Orientation__orientation, 'N')
+        self.assertIsNone(obstacles)
+
+    def testExecCommandsObstacle(self):
+        self.mars = planet.Planet(5, 5, [obstacle.Obstacle(None, position.Position(0, 5))])
+        commands = command.Command(['F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'], True)
+
+        self.curiosity, obstacles = commands.exec_commands(self.mars, self.curiosity)
+
+        self.assertEqual(self.curiosity._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.curiosity._Rover__position._Position__y._Coordinate__value, 4)
+        self.assertEqual(self.curiosity._Rover__orientation._Orientation__orientation, 'N')
+        self.assertEqual(obstacles, [0, 5])
 
     def tearDown(self):
         # Close the client connection
@@ -69,4 +101,4 @@ class CommandTests(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
