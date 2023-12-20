b43c9e9e33a97f2fbb1b159df565350bab2375fd
GMICHON <gauthier.michon@ynov.com>
2023-12-20 12:39:00 +0100

    add tests

---
+++ Tests/CommandTests.py
@@ -0,0 +1,110 @@
+from unittest import TestCase, mock, main
+import time
+import threading
+import sys
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(root_dir)
+from Domain.Topologie import position
+from Domain.Exploration import planet, obstacle
+from Domain.MissionRover import rover, command
+import Rover.main as r
+import MissionControl.main as mc
+
+class TestServer:
+    def __init__(self):
+        self.sender = r.SenderToMissionControl()
+        self.receiver = r.ReceiverFromMissionControl()
+        self.protocol = r.MyCommunicationProtocol(self.sender, self.receiver)
+
+
+class TestClient:
+    def __init__(self):
+        self.sender = mc.SenderToRover()
+        self.receiver = mc.ReceiverFromRover()
+        self.protocol = mc.MyCommunicationProtocol(self.sender, self.receiver)
+
+
+class CommandTests(TestCase):
+    def setUp(self):
+        self.server_address = ('127.0.0.1', 12345)
+        self.server = TestServer()
+        self.client = TestClient()
+        self.mars = planet.Planet(5, 5, None)
+        self.curiosity = rover.Rover(0, 0, 'N')
+
+        # Start the server in a separate thread
+        self.server_thread = threading.Thread(target=self.server.protocol.initialize_server,
+                                              args=(self.server_address,))
+        self.server_thread.start()
+
+        # Allow some time for the server to start before connecting the client
+        time.sleep(1)
+
+        # Connect the client to the server
+        self.client.protocol.establish_connection(self.server_address)
+
+    def testAddCommandsValidInput(self):
+        commands = command.Command()
+
+        # Use mock to simulate user input
+        with mock.patch('builtins.input', return_value='FFLBR'):
+            commands.add_command()
+
+            self.assertEqual(commands._Command__command_order, ['F', 'F', 'L', 'B', 'R'])
+            self.assertEqual(commands._Command__is_valid, True)
+
+    def testAddCommandsInvalidInput(self):
+        commands = command.Command()
+
+        # Use mock to simulate user input
+        with mock.patch('builtins.input', return_value='invalid_input'):
+            commands.add_command()
+
+            self.assertEqual(commands._Command__command_order, [])
+            self.assertEqual(commands._Command__is_valid, False)
+
+    def testExecCommandsValid(self):
+        commands = command.Command(['F', 'F', 'L', 'B', 'R'], True)
+
+        self.curiosity, obstacles = commands.exec_commands(self.mars, self.curiosity)
+
+        self.assertEqual(self.curiosity._Rover__position._Position__x._Coordinate__value, 1)
+        self.assertEqual(self.curiosity._Rover__position._Position__y._Coordinate__value, 2)
+        self.assertEqual(self.curiosity._Rover__orientation._Orientation__orientation, 'N')
+        self.assertIsNone(obstacles)
+
+    def testExecCommandsInvalid(self):
+        commands = command.Command(['invalid_input'], False)
+
+        self.curiosity, obstacles = commands.exec_commands(self.mars, self.curiosity)
+
+        self.assertEqual(self.curiosity._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.curiosity._Rover__position._Position__y._Coordinate__value, 0)
+        self.assertEqual(self.curiosity._Rover__orientation._Orientation__orientation, 'N')
+        self.assertIsNone(obstacles)
+
+    def testExecCommandsObstacle(self):
+        self.mars = planet.Planet(5, 5, [obstacle.Obstacle(None, position.Position(0, 5))])
+        commands = command.Command(['F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'], True)
+
+        self.curiosity, obstacles = commands.exec_commands(self.mars, self.curiosity)
+
+        self.assertEqual(self.curiosity._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.curiosity._Rover__position._Position__y._Coordinate__value, 4)
+        self.assertEqual(self.curiosity._Rover__orientation._Orientation__orientation, 'N')
+        self.assertEqual(obstacles, [0, 5])
+
+    def tearDown(self):
+        # Close the client connection
+        self.client.protocol.close_connection()
+
+        # Wait for the server thread to finish
+        self.server_thread.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Tests/RoverTests.py
@@ -0,0 +1,86 @@
+import unittest
+import sys
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(root_dir)
+from Domain.MissionRover.rover import Rover
+from Domain.Exploration.planet import Planet
+from Domain.MissionRover.command import Command
+from Domain.Topologie.position import Position
+from Domain.Exploration.obstacle import Obstacle
+
+
+class TestRover(unittest.TestCase):
+    def setUp(self):
+        self.planet = Planet(5, 5, [])
+        self.rover = Rover(0, 0, 'N')
+
+    def testForward(self):
+        self.rover = self.rover.move_forward(self.planet)
+        self.assertEqual(self.rover._Rover__position
+                         ._Position__x
+                         ._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 1)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'N')
+
+    def testForwardOutOfLimit(self):
+        self.rover = Rover(0, 5, 'N')
+
+        self.rover = self.rover.move_forward(self.planet)
+        self.assertEqual(self.rover._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'N')
+
+    def testBackwardOutOfLimit(self):
+        self.rover = self.rover.move_backward(self.planet)
+        self.assertEqual(self.rover._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 5)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'N')
+
+    def testBackward(self):
+        self.rover = Rover(0, 4, 'N')
+
+        self.rover = self.rover.move_backward(self.planet)
+        self.assertEqual(self.rover._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 3)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'N')
+
+    def testTurnRight(self):
+        self.rover = self.rover.turn_right()
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'E')
+
+    def testTurnLeft(self):
+        self.rover = self.rover.turn_left()
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'W')
+
+    def testForwardWithInstruction(self):
+        commands = Command(['F'], True)
+        self.rover, _ = commands.exec_commands(self.planet, self.rover)
+        self.assertEqual(self.rover._Rover__position
+                         ._Position__x
+                         ._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 1)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'N')
+
+    def testBackwardWithInstruction(self):
+        self.rover = Rover(0, 4, 'N')
+
+        commands = Command(['B'], True)
+        self.rover, _ = commands.exec_commands(self.planet, self.rover)
+        self.assertEqual(self.rover._Rover__position._Position__x._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 3)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'N')
+
+    def testInstructions(self):
+        commands = Command(['L', 'L', 'F', 'R', 'F', 'F', 'L', 'B', 'L', 'F', 'R'], True)
+        self.rover, _ = commands.exec_commands(self.planet, self.rover)
+        self.assertEqual(self.rover._Rover__position._Position__x._Coordinate__value, 5)
+        self.assertEqual(self.rover._Rover__position._Position__y._Coordinate__value, 0)
+        self.assertEqual(self.rover._Rover__orientation._Orientation__orientation, 'S')
+
+
+if __name__ == '__main__':
+    unittest.main()
